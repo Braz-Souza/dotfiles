a369a317533b8e271a471430b6625f1377907a94
Braz Souza <brazsdsgabriel@gmail.com>
2024-06-27 17:33:01 -0300

    Changed workspace icons

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { " <terminal>", "󰈹 <browser>", "󰍩 <messages>", " <learn>", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
