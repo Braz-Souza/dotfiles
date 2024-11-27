02706924fd9f06df1b323d275c98f60f50911f22
Braz Souza <brazsdsgabriel@gmail.com>
2024-11-26 21:35:10 -0300

    Change Screenshot scripts to save images in clipboard selection

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-mkdir -p /home/vazio/Documents/Screenshots && scrot /home/vazio/Documents/Screenshots/%m-%d-%Y-%H%M%S.png
+
+NEW_FILE="$(date +'%m-%d-%Y-%H%M%S').png"
+mkdir -p /home/vazio/Documents/Screenshots && scrot /home/vazio/Documents/Screenshots/$NEW_FILE &&
+  xclip -selection clipboard -t image/png -i /home/vazio/Documents/Screenshots/$NEW_FILE
--- scripts/screenshotsel.sh
+++ scripts/screenshotsel.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-mkdir -p /home/vazio/Documents/Screenshots && scrot /home/vazio/Documents/Screenshots/%m-%d-%Y-%H%M%S.png --select --line mode=edge
+
+NEW_FILE="$(date +'%m-%d-%Y-%H%M%S').png"
+mkdir -p /home/vazio/Documents/Screenshots && scrot /home/vazio/Documents/Screenshots/$NEW_FILE --select --line mode=edge &&
+  xclip -selection clipboard -t image/png /home/vazio/Documents/Screenshots/$NEW_FILE
