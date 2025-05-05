3c880a1b86a6184f5e681a1c9b1ca35ab69c3166
Braz Souza <brazsdsgabriel@gmail.com>
2025-05-05 08:31:02 -0300

    Fix: Changed my own vazio to tilde expansion

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 NEW_FILE="$(date +'%m-%d-%Y-%H%M%S').png"
-mkdir -p /home/vazio/Documents/Screenshots && scrot /home/vazio/Documents/Screenshots/$NEW_FILE &&
-  xclip -selection clipboard -t image/png -i /home/vazio/Documents/Screenshots/$NEW_FILE
+mkdir -p ~/Documents/Screenshots && scrot ~/Documents/Screenshots/$NEW_FILE &&
+  xclip -selection clipboard -t image/png -i ~/Documents/Screenshots/$NEW_FILE
