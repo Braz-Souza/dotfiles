a4ae7cd9a2a89212a08cc99abd8bc27ba2e56f63
Braz Souza <brazsdsgabriel@gmail.com>
2025-05-05 08:26:38 -0300

    Add script to change backlight

--- scripts/aliases-usb.sh
+++ scripts/aliases-usb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "alias hiraku='sudo sh ~/dotfiles/scripts/open-usb.sh'" >> ~/.bashrc
 echo "alias shuuryou='sudo sh ~/dotfiles/scripts/close-usb.sh'" >> ~/.bashrc
+echo "alias brightlevel='~/.dotfiles/scripts/brightscript.sh'" >> ~/.bashrc
 echo "Please, to use the alias predefined by MYSELF in this terminal use source ~/.bashrc"
 echo "The hiraku command executes MY predefined open-usb command designed to decrypt and mount MY usb flash drive"
 echo "The shuuryou command executes MY predefined close-usb design to umount and crypt MY usb flash drive"
